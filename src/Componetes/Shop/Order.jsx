022ed985165718b4d9237d71bd7980c55d82291d
Md Minhaj <parsonal322532@gmail.com>
2023-12-12 23:33:16 +0600

    Order Summary Done

--- src/Componetes/Shop/Order.jsx
+++ src/Componetes/Shop/Order.jsx
@@ -1,18 +1,29 @@
 import DeleteImg from '../../assets/images/Vector.svg';
 import ArrowImg from '../../assets/images/Arrow.svg';
 
-const Order = (props) => {
+const Order = ({productCart, clearCart}) => {
+    let totalPrice = 0;
+    let shippingCost = 0;
+
+    for(const product of productCart){
+        totalPrice = totalPrice + product.price;
+        shippingCost = shippingCost + product.shipping;
+    }
+
+    const tax = totalPrice * 7 / 100;
+    const grandTotal = totalPrice + shippingCost + tax;
+
     return (
         <div className="px-6">
             <h1 className="text-center mt-6 text-gray-800 text-[25px] font-normal font-['Lato']">Order Summary</h1>
             <div className="py-10 flex flex-col gap-4">
-                <p className=" text-slate-700 text-[17px] font-normal font-['Lato']">Selected Items: {props.productItems}</p>
-                <p className=" text-slate-700 text-[17px] font-normal font-['Lato']">Total Price: ${props.total}</p>
-                <p className=" text-slate-700 text-[17px] font-normal font-['Lato']">Total Shipping Charge: ${props.charge}</p>
-                <p className=" text-slate-700 text-[17px] font-normal font-['Lato']">Tax: ${props.tax}</p>
-                <h4 className=" text-neutral-900 text-[21px] font-normal font-['Lato']">Grand Total: ${props.grandTotal}</h4>
+                <p className=" text-slate-700 text-[17px] font-normal font-['Lato']">Selected Items: {productCart.length}</p>
+                <p className=" text-slate-700 text-[17px] font-normal font-['Lato']">Total Price: ${totalPrice}</p>
+                <p className=" text-slate-700 text-[17px] font-normal font-['Lato']">Total Shipping Charge: ${shippingCost}</p>
+                <p className=" text-slate-700 text-[17px] font-normal font-['Lato']">Tax: ${tax.toFixed(2)}</p>
+                <h4 className=" text-neutral-900 text-[21px] font-normal font-['Lato']">Grand Total: ${grandTotal.toFixed(2)}</h4>
             </div>
-            <button onClick={props.clearCart} className='flex items-center gap-3 justify-center w-full h-12 bg-red-500 rounded text-white'>
+            <button onClick={clearCart} className='flex items-center gap-3 justify-center w-full h-12 bg-red-500 rounded text-white'>
                 Clear Cart 
                 <img src={DeleteImg} alt="" />
             </button>
--- src/Componetes/Shop/Shop.jsx
+++ src/Componetes/Shop/Shop.jsx
@@ -6,43 +6,20 @@ import Order from "./Order";
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [productCart, setProductCart] = useState([]);
-    const [total, setTotal] = useState(0);
-    const [charge, setCharge] = useState(0);
-    const [tax, setTax] = useState(0);
-    const [grandTotal, setGrandTotal] = useState(0);
 
     useEffect(()=>{
         fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
     }, []);
-
+    
     const addToCart = (card) =>{
         const newCard = [...productCart, card];
         setProductCart(newCard);
-        
-        const newCharge = newCard.length * 5;
-        setCharge(newCharge);
-        
-        newCard.map(product =>{
-            const productTotalPrice = product.price + total;
-            setTotal(productTotalPrice);
-            const taxTotalCal = productTotalPrice + newCharge;
-            const taxValue = (taxTotalCal / 100) * 10;
-            setTax(taxValue);
-
-            // Grand Total
-            const grandTotal = productTotalPrice + newCharge + parseInt(taxValue);
-            setGrandTotal(grandTotal);
-        });
     }
 
-    const clearCart = () =>{
+    const clearCart = ()=>{
         setProductCart([]);
-        setTotal(0);
-        setCharge(0);
-        setTax(0);
-        setGrandTotal(0);
     }
 
     return (
@@ -53,7 +30,7 @@ const Shop = () => {
             }
             </div>
             <div className="w-[25rem] h-screen bg-[#FF99004D] fixed right-0">
-                <Order productItems={productCart.length} total={total} charge={charge} tax={parseInt(tax)} grandTotal={grandTotal} clearCart={clearCart}></Order>
+                <Order productCart={productCart} clearCart={clearCart}></Order>
             </div>
         </div>
     );

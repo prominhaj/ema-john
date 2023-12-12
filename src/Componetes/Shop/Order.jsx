320af71313cef92dd24dca674cac71f6217c5379
Md Minhaj <parsonal322532@gmail.com>
2023-12-13 01:51:25 +0600

    Order Summary Done And Fix

--- src/Componetes/Shop/Order.jsx
+++ src/Componetes/Shop/Order.jsx
@@ -4,10 +4,15 @@ import ArrowImg from '../../assets/images/Arrow.svg';
 const Order = ({productCart, clearCart}) => {
     let totalPrice = 0;
     let shippingCost = 0;
+    let quantity = 0;
 
     for(const product of productCart){
-        totalPrice = totalPrice + product.price;
+        // if(product.quantity === 0){
+        //     product.quantity = 1;
+        // }
+        totalPrice = totalPrice + product.price * product.quantity;
         shippingCost = shippingCost + product.shipping;
+        quantity = quantity + product.quantity;
     }
 
     const tax = totalPrice * 7 / 100;
@@ -17,7 +22,7 @@ const Order = ({productCart, clearCart}) => {
         <div className="px-6">
             <h1 className="text-center mt-6 text-gray-800 text-[25px] font-normal font-['Lato']">Order Summary</h1>
             <div className="py-10 flex flex-col gap-4">
-                <p className=" text-slate-700 text-[17px] font-normal font-['Lato']">Selected Items: {productCart.length}</p>
+                <p className=" text-slate-700 text-[17px] font-normal font-['Lato']">Selected Items: {quantity}</p>
                 <p className=" text-slate-700 text-[17px] font-normal font-['Lato']">Total Price: ${totalPrice}</p>
                 <p className=" text-slate-700 text-[17px] font-normal font-['Lato']">Total Shipping Charge: ${shippingCost}</p>
                 <p className=" text-slate-700 text-[17px] font-normal font-['Lato']">Tax: ${tax.toFixed(2)}</p>
--- src/Componetes/Shop/Shop.jsx
+++ src/Componetes/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Product from "./Product/Product";
 import Order from "./Order";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -13,9 +14,37 @@ const Shop = () => {
         .then(data => setProducts(data))
     }, []);
     
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const newArray = [];
+        for(const id in storedCart){
+            const addedProduct = products.find(product => product.id === id);
+
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                newArray.push(addedProduct);
+            }
+        }
+        setProductCart(newArray);
+
+    }, [products])
+
     const addToCart = (card) =>{
-        const newCard = [...productCart, card];
-        setProductCart(newCard);
+        let newCart = [];
+        // const newCard = [...productCart, card];
+        const exists = productCart.find(pd => pd.id === card.id);
+        if(!exists){
+            card.quantity = 1;
+            newCart = [...productCart, card];
+        }
+        else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = productCart.filter(pd => pd.id !== card.id);
+            newCart = [...remaining, exists];
+        }
+        setProductCart(newCart);
+        addToDb(card.id);
     }
 
     const clearCart = ()=>{
